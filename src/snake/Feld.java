7f3f9e7704f5c9042c1bc5aed039582c1b8b535a
Oskar <Oskar@10.91.96.89>
2017-02-17 10:45:48 +0100

    Menue

--- src/snake/Feld.java
+++ src/snake/Feld.java
@@ -1,19 +1,13 @@
 package snake;
 
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+
 
 public class Feld {
 
+	public Feld(){
+		Feld[][] spielFeld = new Feld[10][10];
+		
+	}
 	
-//    public void start(Stage primaryStage){
-//        Pane pane=new Pane();
-//        Scene scene=new Scene(800,600);
-//        primaryStage.setScene(scene);
-//        primaryStage.setTitle("Snake Game");
-//        primaryStage.show();
-//        
-//    }
     
 }
---
+++ src/snake/Menue.java
@@ -0,0 +1,36 @@
+package snake;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Menue extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Snake");
+        Button btn = new Button();
+        btn.setText("Start");
+      
+//        Aktion für den Button setzen 
+        
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//        	@Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
