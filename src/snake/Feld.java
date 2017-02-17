4b8ec07034a94c9cac9a135ee8a0dbc53542132c
Oskar <Oskar@10.91.96.89>
2017-02-17 09:14:50 +0100

    ...

---
+++ src/snake/Feld.java
@@ -0,0 +1,19 @@
+package snake;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Feld {
+
+	
+//    public void start(Stage primaryStage){
+//        Pane pane=new Pane();
+//        Scene scene=new Scene(800,600);
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("Snake Game");
+//        primaryStage.show();
+//        
+//    }
+    
+}
