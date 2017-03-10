5bace8496ab14df70707284936ecf63789945662
Verleih50 <Verleih50@10.91.56.236>
2017-03-10 16:36:22 +0100

    Snake Arraylist zugefügt SnakeBody ist ein Circle lastPart zeigt die größe der List.

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -4,7 +4,9 @@ public class Main {  //hallo test
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		snake s = new snake();
+		s.addBodyPart();
+		System.out.println(s.toString());
 	}
 
 }
--- src/snake/food.java
+++ src/snake/food.java
@@ -2,19 +2,27 @@ package snake;
 
 import javafx.application.Application;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class food extends Application{
 	
 	
-	private Image cherry;
-	private Image apple;
-	private Image banana;
+	private ImageView cherry;
+	
+	public food(){
+	Image img = new Image("cherry.gif");
+	cherry.setImage(img);
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	
+	public void generatePos(int x, int y){
+		cherry.setTranslateX(x);
+		cherry.setTranslateY(y);
+		cherry.setVisible(true);
+	}
 }
