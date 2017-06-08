a829e722356f6df17dda844d2b12005ff226198e
Syynex <oskareichhorn@gmail.com>
2017-06-08 17:50:13 +0200

    .credits

--- src/snake/Credits.java
+++ src/snake/Credits.java
@@ -3,6 +3,8 @@ package snake;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -11,40 +13,30 @@ public class Credits extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {		
 			
-			Text text = new Text();
+			Text text1 = new Text();
 			Text text2 = new Text();
 			Text text3 = new Text();
  			//Setting the text to be added. 
-			text.setText("Fero"); 
+			text1.setText("Fero"); 
 			text2.setText("Oskar");
 			text3.setText("Made by:");
-			//setting the position of the text 
-	     	text.setX(80); 
-			text.setY(35); 
-	        
-			text2.setX(80); 
-			text2.setY(55);
 			
-			text3.setX(5); 
-			text3.setY(15);
 			
-			text.setStyle("-fx-font: 15 arial");
-			text2.setStyle("-fx-font: 15 arial");
-			text3.setStyle("-fx-font: 15 arial");
-			//Creating a Group object  
-			Group root = new Group(text, text2, text3);   
+			text1.setStyle("-fx-font: 25 arial; -fx-text-fill: #FFFFFF;");
+			text2.setStyle("-fx-font: 25 arial; -fx-text-fill: #FFFFFF;");
+			text3.setStyle("-fx-font: 25 arial; -fx-text-fill: #FFFFFF;");
 			
+			
+			text3.setTranslateY(-35); 
+			text2.setTranslateY(+35); 
 	               
-			//Creating a scene object 
-	     	Scene scene = new Scene(root, 150, 100);  
-	     	
-	     	//Setting title to the Stage 
-	     	stage.setTitle("Credits"); 
-	         
-	     	//Adding scene to the stage 
-	     	stage.setScene(scene); 
-	         
-	     	//Displaying the contents of the stage 
+			StackPane root = new StackPane();
+			root.getChildren().add(text1);
+			root.getChildren().add(text2);
+			root.getChildren().add(text3);
+			stage.setScene(new Scene(root, 300, 250)); 
+			root.setStyle("-fx-background-color: #001E32");
+	     	stage.setTitle("Credits");
 	     	stage.centerOnScreen();
 	     	stage.show(); 
 	     
